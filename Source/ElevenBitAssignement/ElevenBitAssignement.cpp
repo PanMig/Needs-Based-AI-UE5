e0d1e2aed06cd65f6a3fc140d8a33047c4f51986
Panagiotis Migkotzidis <panagiotismigo@gmail.com>
2023-10-16 17:24:30 +0300

    Rename project

--- Source/ElevenBitAssignement/ElevenBitAssignement.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ElevenBitAssignement.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ElevenBitAssignement, "ElevenBitAssignement" );
- 
---
+++ Source/NeedsBasedAI/NeedsBasedAIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NeedsBasedAIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANeedsBasedAIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANeedsBasedAIGameMode();
+};
+
+
+
